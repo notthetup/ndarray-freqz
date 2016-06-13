fbaf69873a9e030243501e7bc1f151b66b2eb318
Chinmay Pendharkar <notthetup@gmail.com>
2016-06-13 10:47:00 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var horner = require("horner");
+var ndarray = require("ndarray");
+var cops = require("ndarray-complex");
+
+/*
+* Given the numerator b and denominator a of a digital filter, compute its frequency response
+*/
+module.exports = function(b , a){
+
+}
